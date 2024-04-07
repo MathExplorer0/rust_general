6472220ef65b2f989baaae7f4ecc74ef6d7ec0d8
MathExplorerGH <mathexplorer@tutamail.com>
2024-04-08 00:03:49 +0100

    nihililm moment

--- learning_basics/src/main.rs
+++ learning_basics/src/main.rs
@@ -1,3 +1,8 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number !");
+    println!("Please input your guess: ");
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess).expect("failed to read the message");
+    println!("You guessed: {guess}");
 }
