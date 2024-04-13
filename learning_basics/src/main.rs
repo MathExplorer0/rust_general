5ff15e33158c5e19eb2bba351306c3cfb8b6b175
MathExplorerGH <mathexplorer@tutamail.com>
2024-04-13 06:33:12 +0100

    arrray program

--- learning_basics/src/main.rs
+++ learning_basics/src/main.rs
@@ -1,6 +1,21 @@
+use std::io;
 
 fn main() {
-    let a = [9; 200];
+    let a = [1, 2, 3, 4, 5];
+    let mut state = true;
+    
+    while state {
+        println!("please enter the array index");
 
-    println!("sorry about the su*a bl*at <3");
+        let mut index = String::new();
+        io::stdin().read_line(&mut index).expect("failed to read the line");
+        let index: usize = index.trim().parse().expect("the input is not a number");   
+        if index <= a.len()-1 {
+            let element = a[index]; 
+            println!("the element of the array index {index} is {element}");   
+            state = false;
+        }else {
+            println!("Err the index {index} is out of bound, indexes allowed: 0-{}", a.len()-1);
+        } 
+    } 
 }
