921bbdc84af12d4462c3b3af48810788df0e109d
MathExplorerGH <mathexplorer@tutamail.com>
2024-04-14 13:24:26 +0100

    sorting algo with no functions

--- learning_basics/src/main.rs
+++ learning_basics/src/main.rs
@@ -1,8 +1,21 @@
 fn main() {
-    let mut countdown = 10;
-    while countdown != 0 {
-        println!("{countdown}");
-        countdown -= 1;
+    //sorting algorithm from small to big
+    let mut array = [10, 7, 3, 0, 4, 2, 9];
+    
+    for i in 0..array.len() - 1{
+        for j in 0..array.len() - 1{
+            
+            if array[j] > array[j+1] {
+                let temp = array[j+1];
+                array[j+1] = array[j];
+                array[j] = temp;         
+            } 
+        }
+        println!("iteration {}", i + 1);
+        for x in 0..array.len() {
+            print!("{}  ", array[x]);
+        }
+        println!("");
     }
-    println!("LIFTOFF !");
+
 }
