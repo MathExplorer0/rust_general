50e7f8d05ea62e7f19f6fff4a23412e20bbb8c71
MathExplorerGH <mathexplorer@tutamail.com>
2024-04-14 12:47:59 +0100

    functions !

--- learning_basics/src/main.rs
+++ learning_basics/src/main.rs
@@ -1,21 +1,9 @@
 use std::io;
 
 fn main() {
-    let a = [1, 2, 3, 4, 5];
-    let mut state = true;
-    
-    while state {
-        println!("please enter the array index");
+    println!("yo bro {}", addition(69, 11));
+}
 
-        let mut index = String::new();
-        io::stdin().read_line(&mut index).expect("failed to read the line");
-        let index: usize = index.trim().parse().expect("the input is not a number");   
-        if index <= a.len()-1 {
-            let element = a[index]; 
-            println!("the element of the array index {index} is {element}");   
-            state = false;
-        }else {
-            println!("Err the index {index} is out of bound, indexes allowed: 0-{}", a.len()-1);
-        } 
-    } 
+fn addition(x: i32, y: i32) -> i32 {
+    x + y
 }
