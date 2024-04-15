ca994a3a131c0ff255c6ef0b9d138cd8e95638b4
MathExplorerGH <mathexplorer@tutamail.com>
2024-04-15 19:10:00 +0100

    progress calculator

--- calculator_proj/src/main.rs
+++ calculator_proj/src/main.rs
@@ -1,5 +1,31 @@
 use std::io;
 
-fn main() {
+fn main() { 
+    println!("Hello and welcome to the MATH calculator !");
     
+    let i1 = get_input();
+    println!("input: {}", i1);
+}
+
+fn get_input() -> u64 {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).expect("ERROR");
+    let buffer: u64 = buffer.trim().parse().expect("please type a number!");
+    buffer
+}
+
+fn addition(x: u64, y: u64) -> u64 {
+    x + y
+}
+
+fn subtraction(x: u64, y: u64) -> u64 {
+    x - y
+}
+
+fn multiplication(x: u64, y: u64) -> u64 {
+    x * y
+}
+
+fn division(x: u64, y: u64) -> u64 {
+    x / y
 }
