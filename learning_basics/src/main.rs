140505b5e483620c9013ee26cb044aba8cbf8283
MathExplorerGH <mathexplorer@tutamail.com>
2024-04-15 16:09:04 +0100

    calculator project

--- learning_basics/src/main.rs
+++ learning_basics/src/main.rs
@@ -1,10 +1,11 @@
 fn main() {
-    //References and Borrowing
-    let s = String::from("hello");
-    let size = size_calc(&s);
-    println!("{}", size);
-}
+    let mut s = String::from("hello");
 
-fn size_calc(x: &String) -> usize {
-    x.len()
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    println!("{} and {}", r1, r2);
+    
+    let r3 = &mut s; // no problem
+    r3.insert_str(0, "bar");
+    println!("{}", s);
 }
