4d7aa1d37474ca919a79cbddbc26a799990f6204
MathExplorerGH <mathexplorer@tutamail.com>
2024-04-15 06:17:22 +0100

    next is ownership ! very interesting

--- learning_basics/src/main.rs
+++ learning_basics/src/main.rs
@@ -1,21 +1,13 @@
 fn main() {
-    //sorting algorithm from small to big
-    let mut array = [10, 7, 3, 0, 4, 2, 9];
-    
-    for i in 0..array.len() - 1{
-        for j in 0..array.len() - 1{
-            
-            if array[j] > array[j+1] {
-                let temp = array[j+1];
-                array[j+1] = array[j];
-                array[j] = temp;         
-            } 
-        }
-        println!("iteration {}", i + 1);
-        for x in 0..array.len() {
-            print!("{}  ", array[x]);
-        }
-        println!("");
-    }
+    let mut counter = 0;
 
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {result}");
 }
