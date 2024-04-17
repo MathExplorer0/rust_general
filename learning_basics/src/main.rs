431cc0a5e635dfa442499a82d56b9b6db7ad5121
MathExplorerGH <mathexplorer@tutamail.com>
2024-04-17 18:33:25 +0100

    learning HELIX

--- learning_basics/src/main.rs
+++ learning_basics/src/main.rs
@@ -1,21 +1,3 @@
 fn main() {
-    let mut s = String::from("hello world");
-
-    let word = first_word(&s);
-
-    s.clear(); //err ?
-
-    println!("the first word is: {}", word);
-}
-
-fn first_word(s: &String) -> &str {
-    let bytes = s.as_bytes();
-    
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return &s[0..i];
-        }
-    }
-
-    &s[..]
+    println!("Learning HELIX !");
 }
