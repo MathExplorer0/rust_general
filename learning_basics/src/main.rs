8532ba473e3646c3e0dc74f4828742abc9621c9d
MathExplorerGH <mathexplorer@tutamail.com>
2024-04-20 03:20:06 +0100

    stream update

--- learning_basics/src/main.rs
+++ learning_basics/src/main.rs
@@ -1,27 +1,22 @@
-use std::thread::sleep;
-use std::time::Duration;
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
 
-struct Position(f64, f64, f64);
-
-//helix equations implementation in rust
+impl Rectangle {
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+}
 
 fn main() {
-    let is_open = true;
-    let mut count: f64 = 0.0;
-    let mut position_of_point = Position(0.0, 0.0, 0.0);
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
 
-    while is_open {
-        position_of_point.0 = count.cos();
-        position_of_point.1 = count.sin();
-        position_of_point.2 = count;
-
-        count += 0.1;
-
-        sleep(Duration::from_millis(100));
-
-        println!(
-            "x: {:.2}, y: {:.2}, z: {:.2}",
-            position_of_point.0, position_of_point.1, position_of_point.2
-        );
+    if rect1.width() {
+        println!("The rectangle has a nonzero width; it is {}", rect1.width);
     }
 }
