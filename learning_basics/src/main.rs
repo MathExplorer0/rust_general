ea8506dce27d3a16db06a3620dc65557956f3a16
MathExplorerGH <mathexplorer@tutamail.com>
2024-04-23 06:11:24 +0100

    brain fry

--- learning_basics/src/main.rs
+++ learning_basics/src/main.rs
@@ -5,8 +5,12 @@ struct Rectangle {
 }
 
 impl Rectangle {
-    fn width(&self) {
-        println!("True");
+    fn can_hold(&self, rect: &Rectangle) -> bool {
+        if self.width > rect.width && self.height > rect.height {
+            true
+        } else {
+            false
+        }
     }
 }
 
@@ -15,5 +19,15 @@ fn main() {
         width: 30,
         height: 50,
     };
-    rect1.width();
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("can rect1 hold rect2: {}", rect1.can_hold(&rect2));
+    println!("can rect1 hold rect2: {}", rect1.can_hold(&rect3));
 }
