a0140721a2e6df4420c77e1303b893299482eade
MathExplorerGH <mathexplorer@tutamail.com>
2024-04-24 15:24:09 +0100

    just taking a small pause

--- learning_basics/src/main.rs
+++ learning_basics/src/main.rs
@@ -1,33 +1,3 @@
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
-
-impl Rectangle {
-    fn can_hold(&self, rect: &Rectangle) -> bool {
-        if self.width > rect.width && self.height > rect.height {
-            true
-        } else {
-            false
-        }
-    }
-}
-
 fn main() {
-    let rect1 = Rectangle {
-        width: 30,
-        height: 50,
-    };
-    let rect2 = Rectangle {
-        width: 10,
-        height: 40,
-    };
-    let rect3 = Rectangle {
-        width: 60,
-        height: 45,
-    };
-
-    println!("can rect1 hold rect2: {}", rect1.can_hold(&rect2));
-    println!("can rect1 hold rect2: {}", rect1.can_hold(&rect3));
+    println!("Hello Again");
 }
