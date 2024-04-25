05c3aa5fb822d52953f4ee144b6c37d0eb237197
MathExplorerGH <mathexplorer@tutamail.com>
2024-04-25 21:26:07 +0100

    learning how to draw patterns

--- learning_basics/src/main.rs
+++ learning_basics/src/main.rs
@@ -1,3 +1,39 @@
+//WIP
+
 fn main() {
-    println!("Hello Again");
+    println!("Pattern drawing program");
+    //draw_expanding(3);
+    draw_pyramid(3);
+}
+
+// fn draw_expanding(x: u8) {
+//     println!("Expending pattern with {x} lines");
+//     for i in 0..x {
+//         for _j in 0..i + 1 {
+//             print!("*");
+//         }
+//         println!();
+//     }
+// }
+fn draw_pyramid(x: u8) {
+    println!("Pyramid pattern with {} lines", x);
+
+    let mut space = 0;
+
+    if x % 2 == 0 {
+        space = x / 2;
+    } else {
+        space = (x / 2) + 1;
+    }
+
+    for i in 0..x {
+        for _j in 0..space {
+            print!(" ");
+        }
+        space -= 1;
+        for _j in 0..i + 1 {
+            print!("*");
+        }
+        println!();
+    }
 }
