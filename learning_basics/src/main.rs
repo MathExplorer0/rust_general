f768e14b52d1c7600c19cf2ccacb8974199ac514
MathExplorerGH <mathexplorer@tutamail.com>
2024-04-25 22:27:19 +0100

    what am i even doing? it is late i should get some sleep

--- learning_basics/src/main.rs
+++ learning_basics/src/main.rs
@@ -1,39 +1,10 @@
-//WIP
-
 fn main() {
-    println!("Pattern drawing program");
-    //draw_expanding(3);
-    draw_pyramid(3);
-}
+    println!("Pyramid program");
+    let height = 3;
+    let width = height * 2 - 1;
+    let mut space = height / 2 + 1;
 
-// fn draw_expanding(x: u8) {
-//     println!("Expending pattern with {x} lines");
-//     for i in 0..x {
-//         for _j in 0..i + 1 {
-//             print!("*");
-//         }
-//         println!();
-//     }
-// }
-fn draw_pyramid(x: u8) {
-    println!("Pyramid pattern with {} lines", x);
-
-    let mut space = 0;
-
-    if x % 2 == 0 {
-        space = x / 2;
-    } else {
-        space = (x / 2) + 1;
-    }
-
-    for i in 0..x {
-        for _j in 0..space {
-            print!(" ");
-        }
-        space -= 1;
-        for _j in 0..i + 1 {
-            print!("*");
-        }
-        println!();
+    for i in 0..height {
+        for j in 0..width {}
     }
 }
