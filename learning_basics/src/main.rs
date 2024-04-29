404264e76fdfeed29254fa6448add8d2791755b8
MathExplorerGH <mathexplorer@tutamail.com>
2024-04-29 17:32:06 +0100

    Same as learning_c

--- learning_basics/src/main.rs
+++ learning_basics/src/main.rs
@@ -1,20 +1,20 @@
+use std::io;
+
 fn main() {
-    enum Message {
-        Quit,
-        Move { x: i32, y: i32 },
-        Write(String),
-        ChangeColor(i32, i32, i32),
+    let mut input = String::new();
+    let mut x: u128 = 0;
+    let mut y: u128 = 1;
+    let mut z: u128 = 0;
+
+    println!("enter input: ");
+    io::stdin().read_line(&mut input).expect("ERROR READING");
+
+    let input: u32 = input.trim().parse().expect("ERROR TRIM AND PARSE");
+
+    for _i in 0..input + 1 {
+        z = x + y;
+        y = x;
+        x = z;
+        println!("{}", z);
     }
-
-    impl Message {
-        fn call(&self, x: u32) {
-            println!("{x}");
-        }
-    }
-
-    let m = Message::Write(String::from("hello"));
-    m.call(69);
-    let x: Option<u32> = Option::None;
-
-    assert_eq!(x.is_none(), true);
 }
