7832ecb90115f76a48c24cbba51b3e0b6b3671ad
MathExplorerGH <mathexplorer@tutamail.com>
2024-05-13 14:54:40 +0100

    learning some bevy!

--- bevy_basics/src/main.rs
+++ bevy_basics/src/main.rs
@@ -1,35 +1,38 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle, window::WindowResolution};
 
-#[derive(Component)]
-struct Name(String);
+pub struct MainPlug;
 
-#[derive(Component)]
-struct Person;
-
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Vladimir".to_string())));
-    commands.spawn((Person, Name("Olivia".to_string())));
-    commands.spawn((Person, Name("Jack".to_string())));
-}
-
-fn hello_world() {
-    println!("Hello W");
-}
-
-fn greet_people(mut query: Query<&mut Name, With<Person>>) {
-    for mut name in &mut query {
-        if name.0 == "Jack" {
-            name.0 = "ch3ar".to_string();
-            println!("{}", name.0);
-            break;
-        }
-        println!("{}", name.0);
+impl Plugin for MainPlug {
+    fn build(&self, app: &mut App) {
+        app.add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Learning Bevy".to_string(),
+                resolution: WindowResolution::new(600.0, 600.0),
+                resizable: false,
+                ..Default::default()
+            }),
+            ..Default::default()
+        }));
     }
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: meshes.add(Rectangle::default()).into(),
+        transform: Transform::default().with_scale(Vec3::splat(128.0)),
+        material: materials.add(Color::PURPLE),
+        ..default()
+    });
 }
 
 fn main() {
     App::new()
-        .add_systems(Startup, add_people)
-        .add_systems(Update, (hello_world, greet_people))
+        .add_plugins(MainPlug)
+        .add_systems(Startup, setup)
         .run();
 }
